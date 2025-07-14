dc4d97ff7805d3aeff3d34b7b7e75f6cdf9235c1
Firdavs Yuldashev <firdavsyuldashev111@gmail.com>
2025-07-14 21:22:34 +0500

    minor change

--- bot/src/handlers/startHandler.ts
+++ bot/src/handlers/startHandler.ts
@@ -1,25 +1,21 @@
 import { Context } from "grammy"
 
-
-const channelURL = process.env.CHANNEL_KEY!;
-
 export const startHandler = (ctx: Context) => {
    return ctx.reply(`
-    👋 Salom! Bu yerga siz talabalik yillarizda sodir bo'lgan o'z hikoyalaringizni <b>anonim</b> tarzda yuborishingiz mumkin.
+    👋 <b>Salom!</b>
     
-📩 <b>Hikoyangiz "istalgan formatda" bo'lishi mumkin:</b>
+Bu bot orqali siz <b>talabalik yillarida boshdan kechirgan voqealaringizni anonim tarzda yuborishingiz mumkin.</b>
+    
+📩 <b>Hikoyangiz istalgan formatda bo'lishi mumkin:</b>
 - Matn
 - Rasm, video, ovozli xabar
 
 ✨<b>Ixtiyoriy:</b>
-👉 Oxirida universitet nomini yozsangiz, boshqalar uchun qiziqroq va haqiqatga yaqinroq bo'ladi.
+👉 Oxirida <b>universitet nomini</b> yozsangiz, boshqalar uchun qiziqroq va haqiqatga yaqinroq bo'ladi.
 
-<b>📢Hikoyalar ushbu kanalda ulashiladi:</b> ${channelURL} 
+📢 <b>Tanlangan hikoyalar @TalabaSirlari kanalida ulashiladi.</b>
 
-<b>Anonimlik 💯 kafolatlanadi.</b>
-
-🛡️Maxfiylik siyosati bilan ushbu link orqali tanishib chiqishingiz mumkin:
-<a href="https://talabasirlaribot.web.app/privacy">Maxfiylik siyosati</a>
+🛡️<b>Maxfiylik siyosati</b> bilan ushbu link orqali tanishib chiqishingiz mumkin: <a href="https://talabasirlaribot.web.app/privacy">Maxfiylik siyosati</a>
 `, 
     {parse_mode: "HTML",}
   );
